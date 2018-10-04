6f74a661c4645a63f5400fd723ec4d0a1a4ae2b2
MarinaMontag <43826689+MarinaMontag@users.noreply.github.com>
2018-10-04 10:49:03 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include<iostream>;
 using namespace std;
 int main() {
-	double pi = 3.14, R;
-	cin>> R;
-	cout << 2 * pi*R << endl;
-	cout << pi * R*R << endl;
-return 0;
-}
+	int x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	int a = x2 - x1; int b = y1 - y2;
+	cout << "P=2(a+b)="<<2*(a+b)<< endl;
+	cout << "S=ab="<<a*b << endl;
+	return 0;
+	}
